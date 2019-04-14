e21c97cc56f14eb097197d902e53c5a8e54a2779
Nikhil ps <32610794+nikhilpsathyanathan@users.noreply.github.com>
2019-04-15 01:04:53 +0530

    Delete server.js

--- static/js/server.js
+++
@@ -1,8 +0,0 @@
-function wait_msg() {
-   alert("It may take little more time to execute");
-}
-
-function scroll_to() {
-   var element_to_scroll_to = document.getElementById('try');
-   element_to_scroll_to.scrollIntoView();
-}
